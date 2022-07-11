1e395cc3492c607923abe0e4b30f7cef367307c3
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2022-07-11 03:58:28 +0300

    Whitespace refactor

--- lib.js
+++ lib.js
@@ -1,3 +1,6 @@
-export const shouldBeAlive = (interval, useHours = new Date().getHours()) => interval[1] > interval[0] ?
-  useHours > interval[0] && useHours < interval[1] :
-  useHours > interval[0] || useHours < interval[1]
+export const shouldBeAlive = (
+  interval,
+  useHours = new Date().getHours()
+) => interval[1] > interval[0] ?
+    useHours > interval[0] && useHours < interval[1] :
+    useHours > interval[0] || useHours < interval[1]
